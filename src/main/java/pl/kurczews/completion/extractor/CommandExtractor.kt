826c732af312057db31bfe7307cfe880f98b89ac
kurczews <krzysiek.kurczewski@gmail.com>
2019-02-16 15:13:37 +0100

    Minor refactor

--- src/main/java/pl/kurczews/completion/extractor/CommandExtractor.kt
+++ src/main/java/pl/kurczews/completion/extractor/CommandExtractor.kt
@@ -11,18 +11,20 @@ class CommandExtractor {
     fun extract(directedGraph: DirectedAcyclicGraph<String>): List<Command> {
         return directedGraph
                 .getNodes()
-                .filter { !it.startsWith(OPTION_PREFIX) }
+                .filter { isCommand(it) }
                 .map { buildCommand(it, directedGraph) }
-                .filterNot { it.isLeaf() }
+                .filterNot { isLeaf(it) }
     }
 
     private fun buildCommand(command: String, directedGraph: DirectedAcyclicGraph<String>): Command {
         val (subCmds, opts) = directedGraph
                 .getOutgoingNodes(command)
-                .partition { !it.startsWith(OPTION_PREFIX) }
+                .partition { isCommand(it) }
 
         return Command(command, subCmds.toSet(), opts.toSet())
     }
 
-    private fun Command.isLeaf(): Boolean = this.subcommands.isEmpty() && this.options.isEmpty()
+    private fun isLeaf(cmd: Command): Boolean = cmd.subcommands.isEmpty() && cmd.options.isEmpty()
+
+    private fun isCommand(cmd: String) = !cmd.startsWith(OPTION_PREFIX)
 }
