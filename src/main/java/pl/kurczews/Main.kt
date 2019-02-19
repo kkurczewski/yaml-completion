43ab84760098476e280afb4025140be961518b4c
kurczews <krzysiek.kurczewski@gmail.com>
2019-02-19 18:16:17 +0100

    Improve usage

--- src/main/java/pl/kurczews/Main.kt
+++ src/main/java/pl/kurczews/Main.kt
@@ -5,10 +5,21 @@ import java.io.FileReader
 import java.io.FileWriter
 
 fun main(vararg args: String) {
-    try {
-        val writer = FileWriter(args[1])
-        YamlCompletionGraphParser().parseYaml(FileReader(args[0]), writer)
-    } catch (ex: Exception) {
-        System.err.print(ex.toString())
+    if (args.isEmpty()) {
+        System.err.println("Please provide yaml completion as 1st argument!")
+        System.exit(1)
     }
+    try {
+        val inputFile = args[0]
+        val outputFile = args.getOrElse(1) { removeYamlExtension(inputFile) }
+
+        val writer = FileWriter(outputFile)
+        YamlCompletionGraphParser().parseYaml(FileReader(inputFile), writer)
+    } catch (ex: Exception) {
+        System.err.print("Unexpected failure: $ex")
+    }
+}
+
+private fun removeYamlExtension(file: String): String {
+    return file.replace(Regex("\\.ya?ml"), "")
 }
